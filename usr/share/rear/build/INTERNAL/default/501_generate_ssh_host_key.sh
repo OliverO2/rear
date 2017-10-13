f850e5775a7027476aadd50d31ccc9f759a99e83
Oliver Okrongli <oliver.okrongli@infix.de>
2017-10-13 19:40:34 +0200

    Revert: Add SSH private key security as extension for INTERNAL backup method

--- usr/share/rear/build/INTERNAL/default/501_generate_ssh_host_key.sh
+++
@@ -1,6 +0,0 @@
-# Prevent SSH security vulnerabilities:
-# - https://github.com/rear/rear/issues/1511 - Security Vulnerability (Privilege Escalation): SSH private user key disclosure for root account
-# - https://github.com/rear/rear/issues/1512 - Security Vulnerability (Gain Information): SSH private host key disclosure
-# Generate host key for incoming SSH which was excluded from backup by prep/INTERNAL/default/205_secure_ssh_private_keys.sh
-Log "Generating SSH host key"
-ssh-keygen -t ed25519 -N '' -f "$ROOTFS_DIR/etc/ssh/ssh_host_ed25519_key"
--- usr/share/rear/prep/INTERNAL/default/205_secure_ssh_private_keys.sh
+++
@@ -1,4 +0,0 @@
-# Prevent SSH security vulnerabilities:
-# - https://github.com/rear/rear/issues/1511 - Security Vulnerability (Privilege Escalation): SSH private user key disclosure for root account
-# - https://github.com/rear/rear/issues/1512 - Security Vulnerability (Gain Information): SSH private host key disclosure
-COPY_AS_IS_EXCLUDE=( "${COPY_AS_IS_EXCLUDE[@]}" /etc/ssh/ssh_host_* /root/.ssh/id_* )
