06eea0fd78e28d5970e735fb88e1691868651220
Oliver Okrongli <oliver.okrongli@infix.de>
2017-12-21 15:30:51 +0100

    INTERNAL backup: use $TMP_DIR instead of ./build for pyinstaller

--- usr/share/rear/prep/INTERNAL/default/200_prepare_backup_installation.sh
+++ usr/share/rear/prep/INTERNAL/default/200_prepare_backup_installation.sh
@@ -4,10 +4,13 @@ has_binary system-backup || Error "Could not find system-backup script"
 
 [[ -n "$INTERNAL_BACKUP_TARGET_CONFIGURATION" ]] || Error "Configuration variable INTERNAL_BACKUP_TARGET_CONFIGURATION not set"
 
+local work_directory="$TMP_DIR/system-backup.pyinstaller"
+mkdir -p "$work_directory" || Error "Could not create $work_directory"
+
 # Compile the system-backup into a single executable
-pyinstaller --log-level WARN --specpath build --distpath build/dist --onefile --strip "$(which system-backup)"
+(cd "$work_directory" && pyinstaller --log-level WARN --onefile --exclude-module _pytest --exclude-module coverage --exclude-module coverage.cmdline --strip "$(which system-backup)")
 StopIfError "PyInstaller could not bundle system-backup binary"
 
 # Copy system-backup, support programs, and configuration
-PROGS=( "${PROGS[@]}" 'build/dist/system-backup' )
+PROGS=( "${PROGS[@]}" "$work_directory/dist/system-backup" )
 COPY_AS_IS=( "${COPY_AS_IS[@]}" "$INTERNAL_BACKUP_TARGET_CONFIGURATION" )
